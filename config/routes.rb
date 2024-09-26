f0dc2a360e98085798f8eff3007151599d2c9c65
Vignesh Prakasam <vigneshp.ceg@gmail.com>
2024-09-26 11:22:09 +0530

    turbo frames added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :users
 
   root "users#index"
+
+  get '/lorempicsum', to: 'users#lorempicsum'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
